e9fc740c9edb227892af14cb6aaf98bea0f2979d
ahmedredaooooo <ahmedredaqadry@gmail.com>
2023-11-26 03:27:02 +0300

    Fault Handler I #7: env_page_ws_list_create_element() && Fault Handler I #8: Check Invalid Pointers & Fault Handler I #9: Placement    isA

--- kern/trap/trap.c
+++ kern/trap/trap.c
@@ -378,12 +378,10 @@ void fault_handler(struct Trapframe *tf)
 			//TODO: [PROJECT'23.MS2 - #13] [3] PAGE FAULT HANDLER - Check for invalid pointers
 			//(e.g. pointing to unmarked user heap page, kernel or wrong access rights),
 			//your code is here
-			int perms = pt_get_page_permissions(faulted_env->env_page_directory, fault_va);
 
-			if (!(perms & PERM_WRITEABLE) || fault_va >= USER_LIMIT /* || unmarked page*/)
+			uint32 perms = pt_get_page_permissions(faulted_env->env_page_directory,fault_va );
+			if((perms & PERM_PRESENT) && (!(perms & PERM_WRITEABLE) || !(perms & PERM_USER)))
 				sched_kill_env(curenv->env_id);
-
-
 
 			/*============================================================================================*/
 		}
